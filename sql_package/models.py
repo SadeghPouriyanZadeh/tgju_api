36410e3bbab2e4134bc5056b5fafe9303a499221
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2022-01-22 23:57:34 +0330

    change class name to Price

--- sql_package/models.py
+++ sql_package/models.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class Valuable(Base):
+class Price(Base):
     __tablename__ = "prices"
     id = sa.Column(sa.Integer, primary_key=True, index=True)
     gold_18 = sa.Column(sa.Integer)
