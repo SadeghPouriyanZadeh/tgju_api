6b134059e930a0d94b7f087762c29d990ea19c3c
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2022-01-22 23:52:55 +0330

    add PriceRead class

--- sql_package/schemas.py
+++ sql_package/schemas.py
@@ -4,7 +4,16 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class FetchPrice(BaseModel):
+class PriceCreate(BaseModel):
+    name: str
+    gold_18: int
+    gold_coin: int
+    tether: float
+    bitcoin: float
+
+
+class PriceRead(BaseModel):
+    id: int
     name: str
     gold_18: int
     gold_coin: int
